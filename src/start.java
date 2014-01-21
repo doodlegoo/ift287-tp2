622d20ac2423db059beebaba37ce65d04321705a
doodlegoo <vincent.gagnon5@usherbrooke.ca>
2014-01-21 10:11:46 -0500

    premier commit

---
+++ src/start.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+
+public class start{
+
+	public static String s="";
+	
+	
+	public static void afficherMenu(){
+		System.out.println("Application de gestion de cartes de baseball");
+		System.out.println("Voici la liste d'opérations valides : ");
+		System.out.println("1. Ajouter un joueur ");
+		System.out.println("2. Afficher l'information d'un joueur"); 
+		System.out.println("3. Mise à jour de l'information d'un joueur"); 
+		System.out.println("4. Effacer l'information d'un joueur ");
+		System.out.println("5. Liste des joueurs ");
+		System.out.println("6. Sauvegarde ");
+		System.out.println("0. Sortir ");
+		System.out.print("Votre sélection : ");
+		try {
+			s= System.in.read() + "";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(s);
+		
+	}
+	
+	
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		afficherMenu();
+		
+		
+	}
+
+}
