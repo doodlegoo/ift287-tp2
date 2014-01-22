65a0fa2c17638191807caea7e164495089d109e5
Alex Provencher <alex_provencher@videotron.ca>
2014-01-21 22:06:46 -0500

    mod joueurManager

--- src/JoueurManager.java
+++ src/JoueurManager.java
@@ -9,8 +9,29 @@ public class JoueurManager {
 		ListJoueur = new TreeSet<Joueur>();
 	}
 	
+	public void ajouterJoueur(Joueur j)
+	{
+		ListJoueur.add(j);
+	}
+	
+	public void retirerJoueur(Joueur j)
+	{
+		ListJoueur.remove(j);
+	}
+	
+	//retorune la premiere occurance du Joueur dans la liste
+	public Joueur Find(String nom)
+	{
+		for(Joueur j : ListJoueur)
+		{
+			if(nom.equals(j.getPrenom() + " " + j.getNom()))
+				return j;
+		}
+		return null;
+	}
+	
 	//trouver le joueur a modifier
-	public boolean Modifier(String NomJoueur)
+	public boolean modifier(String NomJoueur)
 	{
 		for(Joueur j : ListJoueur)
 		{
@@ -20,5 +41,13 @@ public class JoueurManager {
 			}
 		}
 		return false;
+	}
+	
+	public void afficher()
+	{
+		for(Joueur j : ListJoueur)
+		{
+			j.afficherJoueur();
+		}
 	}
 }
