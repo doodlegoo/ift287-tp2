46dfbab229b19d6581de567743a1d19282c59b3a
Alex Provencher <alex_provencher@videotron.ca>
2014-01-21 22:22:11 -0500

    aaa

--- src/JoueurManager.java
+++ src/JoueurManager.java
@@ -43,16 +43,16 @@ public class JoueurManager {
 		return false;
 	}
 	
-	public void afficherTout()
+	public String afficherTout()
 	{
 		for(Joueur j : ListJoueur)
 		{
-			j.afficherJoueur();
+			return j.afficherJoueur();
 		}
 	}
 	
-	public void afficherJoueur(Joueur j)
+	public String afficherJoueur(Joueur j)
 	{
-		j.afficherJoueur();
+		return j.afficherJoueur();
 	}
 }
