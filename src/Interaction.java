69e02dd126415a47286d7d6821f560720032696e
XutBest <tricolorejusquaubout@gmail.com>
2014-01-28 15:20:19 -0500

    Réaménagement de Interaction et Inventaire

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,12 +1,42 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Interaction {
 	
-	public static void IOmenu()
+	public static void IOmenu(int option)
 	{
-		
+		System.out.print("Option selectionne: " + option + ". ");
+		switch (option) {
+		case 1:
+			System.out.print("Ajouter un joueur\n");
+			break;
+		case 2:
+			System.out.print("Afficher l'information d'un joueur\n");
+			break;
+		case 3:
+			System.out.print("Mise a jour de l'information d'un joueur\n");
+			break;
+		case 5:
+			System.out.print("Liste de joueurs\n");
+			break;
+		case 6:
+			System.out.print("Sauvegarder\n");
+			break;
+		case 0:
+			System.out.print("Sortir\n");
+			break;
+		}
 	}
 	
 	public static Carte IOcarte()
@@ -43,6 +73,71 @@ public class Interaction {
 		return new Carte(titre, equipe, annee);
 		
 		
+	}
+	
+	public static void IOSauvegarder(String fileContent) throws IOException{
+		int nb = 1;
+		Writer writer = null;
+	    try {
+	    	while(new File(System.getProperty("user.dir") + "\\src\\test" + nb + ".txt").exists())
+	    		nb++;
+			writer = new BufferedWriter(new OutputStreamWriter(
+			        new FileOutputStream(System.getProperty("user.dir") + "\\src\\test" + nb + ".txt"), "utf-8"));
+			writer.write(fileContent);
+			System.out.println("Option sélectionné: 6. Sauvegarder\nLe fichier" + System.getProperty("user.dir") + "\\src\\test" + nb + ".txt a ete cree avec succes.");
+		} catch (UnsupportedEncodingException | FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    writer.close();
+	}
+	public static String IOAjoutJoueur(){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Entrez la cle d'identification du joueur: ");
+		String s="";
+		try {
+			s = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+	
+	public static String IOAfficherJoueur(){
+		System.out.println();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Entrez la cle d'identification du joueur: ");
+		String s="";
+		try {
+			s = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+	
+	public static List<String[]> IOInitialisation(){
+		List<String[]> listeJoueurs = new ArrayList<String[]>();
+		try {
+			File file = new File(System.getProperty("user.dir") + "\\src\\test1.txt");
+			if(file.exists()){
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String line;
+				while ((line = br.readLine()) != null) {
+					String[] contenus = line.split(";");
+					for (int i = 0; i < contenus.length; i++) {
+						if (contenus[i].contains("\"")) {
+							contenus[i] = contenus[i].replace("\"", "");
+						}
+					}
+					listeJoueurs.add(contenus);
+				}
+				br.close();
+			}
+				
+		} catch (IOException e){
+			e.printStackTrace();			
+		}
+		return listeJoueurs;
 	}
 	
 	public static void IOjoueur()
--- src/Inventaire.java
+++ src/Inventaire.java
@@ -1,14 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
+import java.util.List;
 
 
 public class Inventaire{
@@ -37,13 +30,13 @@ public class Inventaire{
 		
 		int i = Integer.parseInt(s);
 		switch(i){ 
-			case 1 : ajouterJoueur(); break;
-			case 2 : afficherJoueur(); break;
-			case 3 : System.out.println("Mise a jour"); break;
-			case 4 : System.out.println("Effacer joueur"); break;
-			case 5 : System.out.println("Liste joueurs"); break;
-			case 6 : sauvegarderFichier(); break;
-			case 0 : sauvegarderFichier(); System.exit(0); break;
+			case 1 : Interaction.IOmenu(i); ajouterJoueur(); break;
+			case 2 : Interaction.IOmenu(i); afficherJoueur(); break;
+			case 3 : Interaction.IOmenu(i); break;
+			case 4 : Interaction.IOmenu(i); break;
+			case 5 : Interaction.IOmenu(i); break;
+			case 6 : Interaction.IOmenu(i); sauvegarderFichier(); break;
+			case 0 : Interaction.IOmenu(i); sauvegarderFichier(); System.exit(0); break;
 			default: System.out.println("Rentrer un chiffre entre 0 et 6 svp") ; break;
 		}
 		afficherMenu();
@@ -51,85 +44,29 @@ public class Inventaire{
 	
 	
 	private static void ajouterJoueur(){
-		System.out.println("Option selectionne: 1. Ajouter un joueur");
-		System.out.println();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Entrez la clï¿½ d'identification du joueur: ");
-		String s="";
-		try {
-			s = br.readLine();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		jm.ajouterJoueur(s);
+		jm.ajouterJoueur(Interaction.IOAjoutJoueur());
 	}
 	
 	private static void afficherJoueur() {
-		 System.out.println("Option selectionne: 2. Afficher l'information d'un joueur");
-			System.out.println();
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Entrez la clï¿½ d'identification du joueur: ");
-			String s="";
-			try {
-				s = br.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println(jm.afficherJoueur(s));
+		System.out.println(jm.afficherJoueur(Interaction.IOAfficherJoueur()));
 	}
 	
 	private static void lireFichier(){
-		try {
-			File file = new File(System.getProperty("user.dir") + "\\src\\test1.txt");
-			if(file.exists()){
-				BufferedReader br = new BufferedReader(new FileReader(file));
-				String line;
-				while ((line = br.readLine()) != null) {
-					String[] contenus = line.split(";");
-					for (int i = 0; i < contenus.length; i++) {
-						if (contenus[i].contains("\"")) {
-							contenus[i] = contenus[i].replace("\"", "");
-						}
-					}
-					jm.ajouterJoueur(contenus);
-				}
-				br.close();
-			}
-				
-		} catch (IOException e){
-			// TODO Auto-generated catch block
-			e.printStackTrace();			
+		List<String[]> listeJoueurs = Interaction.IOInitialisation();
+		for (String[] contenu : listeJoueurs) {
+			jm.ajouterJoueur(contenu);
 		}
 	}
 
 	private static void sauvegarderFichier() throws IOException{
-		int nb = 1;
-		Writer writer = null;
-	    try {
-	    	while(new File(System.getProperty("user.dir") + "\\src\\test" + nb + ".txt").exists())
-	    		nb++;
-			writer = new BufferedWriter(new OutputStreamWriter(
-			        new FileOutputStream(System.getProperty("user.dir") + "\\src\\test" + nb + ".txt"), "utf-8"));
-			String fileContent = jm.afficherInfoFichierTexte();
-			writer.write(fileContent);
-			System.out.println("Option sélectionné: 6. Sauvegarder\nLe fichier" + System.getProperty("user.dir") + "\\src\\test" + nb + ".txt a ete cree avec succes.");
-		} catch (UnsupportedEncodingException | FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	    writer.close();
+		Interaction.IOSauvegarder(jm.afficherInfoFichierTexte());
 	}
-	
-	
-	
 	
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		lireFichier();
 		afficherMenu();
 		
